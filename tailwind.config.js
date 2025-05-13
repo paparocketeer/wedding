4d273c98339603a20bb8b845d811517017009975
Миронович Роман <roman.mironovich@am-intech.ru>
2025-05-13 23:38:36 +0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'felidae': ['felidaeregular', 'system-ui', 'sans-serif'],
+        'kaliningrad': ['kaliningrad_historical_gRg', 'system-ui', 'sans-serif'],
+        'laudatio': ['laudatiocregular', 'system-ui', 'sans-serif'],
+        'proxima': ['proxima_nova_problack', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
